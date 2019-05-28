e7f33a3a2b9fd5e16fa768cfc5d805affa0cb6b7
denaSH <denamoean17@gmail.com>
2019-05-28 12:39:53 +0300

    make navbar component

---
+++ client/src/Components/CommonComponent/Navbar/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+const Navbar = () => (
+  <div className="navbar">
+    <div className="controlBoard">لوحة التحكم</div>
+    <div className="circle">D</div>
+  </div>
+);
+
+export default Navbar;
