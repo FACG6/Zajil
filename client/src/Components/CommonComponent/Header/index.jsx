6e9e97a181eab4e1b2d008f86e222612f668b219
Nareman <naremanmohhilles@gmail.com>
2019-05-28 16:13:36 +0300

    resolve commit

--- client/src/Components/CommonComponent/Header/index.jsx
+++ client/src/Components/CommonComponent/Header/index.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './style.css';
 
-const Header = ({ title, icon }) => (
-  <div className="header">
-    <icon className="icon" />
-    <h2>{title}</h2>
-    <hr className="line" />
-  </div>
-);
+const Header = ({ title, icon }) => {
+  return (
+    <div className="header">
+      {icon}
+      <h2>{title}</h2>
+      <hr className="line" />
+    </div>
+  );
+};
 Header.propTypes = {
   icon: PropTypes.node.isRequired,
   title: PropTypes.string.isRequired,
