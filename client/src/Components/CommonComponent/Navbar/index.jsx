05b985d9db07681e81a3f6d56365ebb5e530e73a
denaSH <denamoean17@gmail.com>
2019-05-28 15:16:36 +0300

    change class name

--- client/src/Components/CommonComponent/Navbar/index.jsx
+++ client/src/Components/CommonComponent/Navbar/index.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 const Navbar = ({ name }) => (
   <div className="navbar">
-    <h3 className="controlBoard">لوحة التحكم</h3>
+    <h3 className="control-board">لوحة التحكم</h3>
     <h4 className="logout">تسجيل الخروج</h4>
     <div className="circle">{name}</div>
   </div>
