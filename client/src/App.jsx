b3837c216bb2018578c0154ac62dce42d2ac9d14
Anies <a2nies2012@gmail.com>
2019-05-29 19:18:04 +0300

    linking view icons to captain's and customer's pages and reforming the code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,45 @@
-import React, {Component} from 'react';
-import { viewPopup, editPopup, deletePopup } from './Components/CommonComponent/table/Popups';
-import TableComponent from './Components/CommonComponent/table/Table';
-import './App.css';
-
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  viewPopup,
+  editPopup,
+  deletePopup
+} from "./Components/CommonComponent/table/Popups";
+import TableComponent from "./Components/CommonComponent/table/Table";
+import "./App.css";
 
 class App extends Component {
-  state = {
-    
-  }
+  state = {};
   render() {
     return (
-      <div className="App">
-      <TableComponent
-        pageName="customers"
-        columns={[{
-          key: 54, customer: 'fgd', captain: 'fgdf', date: 'dfsdf',
-        }, {
-          key: 56, customer: 'fgd', captain: 'fgdf', date: 'dfsdf',
-        }]}
-        viewPopup={viewPopup}
-        editPopup={editPopup}
-        deletePopup={deletePopup}
-      />
-    </div>
+      <Router>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <TableComponent
+              pageName="captains"
+              columns={[
+                {
+                  key: 54,
+                  customer: "fgd",
+                  captain: "fgdf",
+                  date: "dfsdf"
+                },
+                {
+                  key: 56,
+                  customer: "fgd",
+                  captain: "fgdf",
+                  date: "dfsdf"
+                }
+              ]}
+              viewPopup={viewPopup}
+              editPopup={editPopup}
+              deletePopup={deletePopup}
+            />
+          )}
+        />
+      </Router>
     );
   }
 }
