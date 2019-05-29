0f08988fd7d39a13bd731b8200f3d03b7a4345c5
shorouq <shrooqabdullahsaad@gmail.com>
2019-05-30 01:05:50 +0300

    create TUser table

--- server/database/config/dbBulid.sql
+++ server/database/config/dbBulid.sql
@@ -35,11 +35,38 @@ BEGIN;
         fk_i_customer_id INTEGER REFERENCES TUser(pk_i_id),
         fk_i_captain_id INTEGER REFERENCES TUser(pk_i_id),
         fk_i_place_id INTEGER REFERENCES places(pk_i_id),
-    	s_customer_address TEXT NOT NULL,
+        s_customer_address TEXT NOT NULL,
         s_customer_phone TEXT NOT NULL,
         dt_create_at DATE DEFAULT current_date,
         dt_delete_at DATE,
         dt_modified_date DATE
+    );
+
+    CREATE TABLE TUser
+    (
+        pk_i_id SERIAL PRIMARY KEY,
+        s_name TEXT NOT NULL,
+        s_mobile_number TEXT NOT NULL,
+        s_email TEXT NOT NULL,
+        b_status BOOLEAN,
+        s_address TEXT NOT NULL,
+        s_access_token TEXT,
+        s_image TEXT NOT NULL,
+        d_latitude FLOAT,
+        d_longitude FLOAT,
+        i_type INTEGER NOT NULL,
+        d_rate FLOAT,
+        b_online BOOLEAN,
+        b_active BOOLEAN,
+        dt_modified_date DATE,
+        dt_create_at DATE DEFAULT current_date,
+        dt_delete_at DATE,
+        s_password TEXT NOT NULL,
+        s_id_number INTEGER,
+        s_attachment TEXT,
+        s_driver_licence_number INTEGER,
+        s_extra_1 TEXT,
+        s_extra_2 TEXT
     );
 
     COMMIT;
