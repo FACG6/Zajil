040b7c00cdc4ecd0107f91c940aee2f548164d21
Jamalat <jamalatshamallakh@gmail.com>
2019-05-29 22:10:55 +0300

    solve conflict

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,35 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import './App.css';
-// import Navbar from './Components/CommonComponent/Navbar';
+import React, {Component} from 'react';
+import {BrowserRouter,Route} from 'react-router-dom';
+import { viewPopup, editPopup, deletePopup } from './Components/CommonComponent/table/Popups';
+import TableComponent from './Components/CommonComponent/table/Table';
 import SideBar from './Components/CommonComponent/Sidebar';
-import Button from './Components/CommonComponent/Button';
+import './App.css';
 
 
-function App() {
-  return (
-    <div className="App">
-      {/* <Navbar /> */}
+class App extends Component {
+  state = {
+    
+  }
+  render() {
+    return (
+      <div className="App">
       <BrowserRouter>
-        <Route path="/" component={SideBar} />
-      </BrowserRouter>
-
-
+      <Route path="/" component={SideBar} />
+    </BrowserRouter>
+      <TableComponent
+        pageName="customers"
+        columns={[{
+          key: 54, customer: 'fgd', captain: 'fgdf', date: 'dfsdf',
+        }, {
+          key: 56, customer: 'fgd', captain: 'fgdf', date: 'dfsdf',
+        }]}
+        viewPopup={viewPopup}
+        editPopup={editPopup}
+        deletePopup={deletePopup}
+      />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
