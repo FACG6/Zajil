4c408498dec988b1ee34c14bd111c9c0ba95c656
Nareman <naremanmohhilles@gmail.com>
2019-05-29 10:09:55 +0300

    solve bem problem

--- client/src/Components/CommonComponent/Header/index.jsx
+++ client/src/Components/CommonComponent/Header/index.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './style.css';
+/* when pass Icon put insilde it <Icon type="..." className="header__icon" /> */
 
-const Header = ({ title, icon }) => {
-  return (
-    <div className="header">
-      {icon}
-      <h2>{title}</h2>
-      <hr className="line" />
-    </div>
-  );
-};
+const Header = ({ title, Icon }) => (
+  <div className="header">
+    {Icon}
+    <h2 className="header__h2">{title}</h2>
+    <hr className="header__line" />
+  </div>
+);
 Header.propTypes = {
-  icon: PropTypes.node.isRequired,
+  Icon: PropTypes.node.isRequired,
   title: PropTypes.string.isRequired,
 };
 export default Header;
