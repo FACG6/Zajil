2435fa9d07838129e61e461a0722cd6a8d859522
shorouq <shrooqabdullahsaad@gmail.com>
2019-05-30 21:13:04 +0300

    add check login in the index of admin

--- server/controller/accountsMangment/admin/index.js
+++ server/controller/accountsMangment/admin/index.js
@@ -1,4 +1,9 @@
 const express = require('express');
 
+const { check } = require('./checkLogin');
+
 const router = express.Router();
+
+router.route('/login')
+  .post(check);
 module.exports = router;
