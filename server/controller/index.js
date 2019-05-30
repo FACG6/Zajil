c8f6c7a6ef468cffa1e9b7aa52f7ad8ac2e9ba6d
shorouq <shrooqabdullahsaad@gmail.com>
2019-05-30 21:11:15 +0300

    add admin as middlware

--- server/controller/index.js
+++ server/controller/index.js
@@ -4,17 +4,14 @@ const express = require('express');
 
 const { check } = require('./middleware/authentication');
 const { protect } = require('./middleware/protectRoute');
+const admin = require('./accountsMangment/admin');
 
 const router = express.Router();
 
 router.use(check);
 // the all routes start from here
-
-router.get('/', (req, res) => {
-  res.send('First setup');
-});
+router.use(admin);
 
 router.use(protect);
 // the protected route start from here
-
 module.exports = router;
