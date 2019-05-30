fa7f5a06769555e24679507984712589b93d3b3d
shorouq <shrooqabdullahsaad@gmail.com>
2019-05-30 09:58:58 +0300

    make database connection for database

---
+++ server/database/config/dbConnection.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+const url = require('url');
+
+let dbURL = process.env.DB_LOCAL;
+
+if (process.env.NODE_ENV === 'production') {
+  dbURL = process.env.DB_HEROKU;
+} else {
+  dbURL = process.env.DB_LOCAL;
+}
+
+if (!dbURL) {
+  throw new Error('No Database !');
+}
+
+const urlParams = url.parse(dbURL);
+const [username, password] = urlParams.auth.split(':');
+
+const option = {
+  user: username,
+  password,
+  database: urlParams.path.split('/')[1],
+  host: urlParams.hostname,
+  port: urlParams.port,
+  ssl: !(urlParams.hostname === 'localhost'),
+};
+
+module.exports = Pool(option);
