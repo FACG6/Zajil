09031be8702467f6f862d853b0b08afa204267a4
shorouq <shrooqabdullahsaad@gmail.com>
2019-05-30 14:05:40 +0300

    add login route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,24 @@
-import React, {Component} from 'react';
-import {BrowserRouter,Route} from 'react-router-dom';
-import { viewPopup, editPopup, deletePopup } from './Components/CommonComponent/table/Popups';
-import TableComponent from './Components/CommonComponent/table/Table';
-import SideBar from './Components/CommonComponent/Sidebar';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import TableComponent from "./Components/CommonComponent/table/Table";
+// import SideBar from "./Components/CommonComponent/Sidebar";
+import Login from "./Components/Layouts/Login";
+import "./App.css";
 
-import Home from './Components/Layouts/Home';
+// import Home from "./Components/Layouts/Home";
 
 class App extends Component {
   state = {
-    
-  }
+
+  };
   render() {
-    return (
-      <div className="App">
-        <Home />
-      <TableComponent
-        pageName="customers"
-        columns={[{
-          key: 54, customer: 'fgd', captain: 'fgdf', date: 'dfsdf',
-        }, {
-          key: 56, customer: 'fgd', captain: 'fgdf', date: 'dfsdf',
-        }]}
-        viewPopup={viewPopup}
-        editPopup={editPopup}
-        deletePopup={deletePopup}
-      /> */}
-    </div>
-    );
+    return <div className="App">
+    <Router>
+      <Switch>
+        <Route path="/login" component={Login} exact />
+      </Switch>
+    </Router>
+    </div>;
   }
 }
 
