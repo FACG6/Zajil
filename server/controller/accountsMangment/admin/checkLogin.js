a35639e2e53aa8cca724d57243544e920b60fd31
shorouq <shrooqabdullahsaad@gmail.com>
2019-05-30 21:12:32 +0300

    make check for username and passowrd

--- server/controller/accountsMangment/admin/checkLogin.js
+++ server/controller/accountsMangment/admin/checkLogin.js
@@ -1,10 +1,35 @@
+const { compareSync } = require('bcryptjs');
+const { sign } = require('jsonwebtoken');
+
 const { select } = require('../../../database/queries/admin/checkAdmin');
 
 exports.check = (req, res) => {
   const { userName, password } = req.body;
-  console.log(req.body);
-  select(userName, password)
-    .then((result) => {
-      console.log(result);
+
+  if (!(userName && password)) {
+    return res.status(400).send({ error: 'Bad Request' });
+  }
+
+  select(userName)
+    .then(({ rowCount, rows }) => {
+      if (!rowCount) {
+        res.status(401).send({ error: 'اسم المستخدم او كلمة السر خاطئة' });
+      } else {
+        const response = compareSync(password, rows[0].password);
+        if (response) {
+          const { id, role, user_name } = rows[0];
+
+          const payload = sign({ id, role, user_name }, process.env.SECRET);
+
+          res.cookie('jwt', payload, { httpOnly: true, maxAge: 60 * 60 * 24 * 7 });
+
+          res.status(200).send({ result: true });
+        } else {
+          res.status(401).send({ error: 'اسم المستخدم او كلمة السر خاطئة' });
+        }
+      }
+    })
+    .catch(() => {
+      res.status(500).send({ error: 'Internal Server Error' });
     });
 };
