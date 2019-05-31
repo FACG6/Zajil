50972d512dede896eb45491c39c7330495a7ec0f
Jamalat <jamalatshamallakh@gmail.com>
2019-05-31 18:05:50 +0300

    change classes name

--- client/src/Components/CommonComponent/Sidebar/index.jsx
+++ client/src/Components/CommonComponent/Sidebar/index.jsx
@@ -6,9 +6,6 @@ import './style.css';
 
 export default class Sidebar extends Component {
   state = {
-   
-  };
-  state = {
     accounts: 'hidden',
     reports: 'hidden',
     msg: 'hidden',
@@ -34,8 +31,6 @@ export default class Sidebar extends Component {
       arrow: 'down'
     },
   }
-
-
   handleClick = (value) => (e) => {
     this.setState(prev => {
       let display = ''; 
@@ -48,257 +43,47 @@ export default class Sidebar extends Component {
         arrow = 'down'
       }
       return {[value]: { display, arrow, }}
-    });
+    })
   }
-  // render() {
+  render() {
       
-  //   const { accounts, reports, msg, settings,arrow_down_accounts,arrow_up_accounts ,arrow_down_reports,arrow_up_reports,arrow_down_msg,arrow_up_msg,arrow_down_settings,arrow_up_settings} = this.state;
-  //   return (
-  //     <div className="container">
+    const { accounts, reports, msg, settings,arrow_down_accounts,arrow_up_accounts ,arrow_down_reports,arrow_up_reports,arrow_down_msg,arrow_up_msg,arrow_down_settings,arrow_up_settings} = this.state;
+    return (
+      <div className="container">
 
-  //       <div className="sidebar">
-  //         <h1 className="sidebar__header">زاجل</h1>
-  //         <div className="sidebar__bar">
-  //           <div className='home'>
-  //             <Link to="/" className="sidebar__links ">
-  //               <span>
-  //                 الرئيسية
-  //           <Icon type="home" style={{marginLeft:'7px'}} />
-  //               </span>
+        <div className="sidebar">
+          <h1 className="sidebar__header">زاجل</h1>
+          <div className="sidebar__bar">
+            <div className='home'>
+              <Link to="/" className="sidebar__links ">
+                <span>
+                  الرئيسية
+            <Icon type="home" style={{marginLeft:'7px'}}/>
+                </span>
 
-  //             </Link>
-  //           </div>
-  //           <div className='home'>
-  //             <Link to="/" className="sidebar__links" >
-  //               <span>
-  //                 إدارة الطلبات
-  //           {' '}
-  //                 <Icon type="menu-unfold" style={{marginLeft:'5px'}}/>
-  //               </span>
-
-  //             </Link>
-  //           </div>
-
-
-  //           <div className="sidebar__dropdown lists" onClick={this.handleClick('accounts')}>
-  //             إدارة الحسابات
-  //         {' '}
-
-  //             <Icon type="tool" />
-  //             {' '}
-  //             <Icon  type={accounts.arrow} style={{marginLeft:'7px',fontSize:'15px'}}/>
-           
-  //           </div>
-  //           <div className={`sidebar__dropdowncontainer ${accounts.display}`}>
-  //               <Link to="/customers" className="sidebar__links ">
-  //                 <span>
-  //                   المستخدمين
-  //             <Icon type="team" style={{marginLeft:'7px'}}/>
-  //                 </span>
-  //               </Link>
-  //               <Link to="/captains" className="sidebar__links">
-  //                 <span>
-  //                   الكابتن
-  //             <Icon type="car" style={{marginLeft:'7px'}}/>
-  //                 </span>
-
-  //               </Link>
-  //               <Link className="sidebar__links">
-  //                 <span>
-  //                   المشرفين
-  //             <Icon type="star" style={{marginLeft:'7px'}} />
-  //                 </span>
-
-  //               </Link>
-  //             </div>
-
-  //           <div onClick={this.handleClick('reports')} className="sidebar__dropdown lists" >
-           
-  //             إدارة التقارير
-  //         {' '}
-  //             <Icon type="profile" />
-  //             <Icon  type={reports.arrow} style={{marginLeft:'7px',fontSize:'15px'}}/>
-  //           </div>
-  //           <div className={`sidebar__dropdowncontainer ${reports.display}`}>
-  //               <Link to="" className="sidebar__links ">
-  //                 <span>
-  //                   تقارير الكابتن
-  //             <Icon type="profile" style={{marginLeft:'7px'}}/>
-  //                 </span>
-
-  //               </Link>
-  //               <Link to="" className="sidebar__links">
-  //                 <span>
-  //                   تقارير المستخدم
-  //             <Icon type="profile" style={{marginLeft:'7px'}} />
-  //                 </span>
-
-  //               </Link>
-  //               <Link to="" className="sidebar__links ">
-  //                 <span>
-  //                   تقارير عامة
-  //             {' '}
-  //                   <Icon type="profile" style={{marginLeft:'7px'}}/>
-  //                 </span>
-
-  //               </Link>
-  //             </div>
-
-  //           <div onClick={this.handleClick('msg')} className="sidebar__dropdown lists" >
-           
-  //             إدارة الرسائل
-  //         <Icon type="mail" />
-  //         <span className={`${arrow_down_msg}`}>
-  //           <Icon type="down" style={{marginLeft:'7px',fontSize:'15px'}}/>
-  //           </span>
-  //           <span className={`${arrow_up_msg}`}>
-  //           <Icon type="up" style={{marginLeft:'7px',fontSize:'15px'}} />
-  //           </span>
-  //           </div>
-  //           <div className={`sidebar__dropdowncontainer ${msg}`}>
-  //               <Link to="" className="sidebar__links ">
-  //                 <span>
-  //                   رسائل قصيرة
-  //             {' '}
-  //                   <Icon type="mail" style={{marginLeft:'7px'}}/>
-  //                 </span>
-
-  //               </Link>
-  //               <Link to="" className="sidebar__links">
-  //                 <span>
-  //                   اشعارات
-  //             {' '}
-  //                   <Icon type="bell"style={{marginLeft:'7px'}} />
-  //                 </span>
-
-  //               </Link>
-  //             </div>
-  //           <div className='home'>
-
-  //             <Link to="" className="sidebar__links">
-  //               <span>
-  //                 إدارة الاستفسارات
-  //           {' '}
-  //                 <Icon type="wechat"style={{marginLeft:'7px'}} />
-  //               </span>
-
-  //             </Link>
-  //           </div>
-  //           <div className='home'>
-  //             <Link to="" className="sidebar__links">
-  //               <span>
-  //                 إدارة البروموكود
-  //           {' '}
-  //                 <Icon type="setting" style={{marginLeft:'5px'}}/>
-  //               </span>
-
-  //             </Link>
-  //           </div>
-
-
-  //           <div onClick={this.handleClick('settings')} className="sidebar__dropdown lists" >
-            
-  //             الإعدادات
-  //         {' '}
-  //             <Icon type="setting" style={{marginLeft:'5px'}}/>
-  //             <span className={`${arrow_down_settings}`}>
-  //           <Icon type="down" style={{marginLeft:'7px',fontSize:'15px'}}/>
-  //           </span>
-  //           <span className={`${arrow_up_settings}`}>
-  //           <Icon type="up" style={{marginLeft:'7px',fontSize:'15px'}} />
-  //           </span>
-  //           </div>
-  //           <div className={`sidebar__dropdowncontainer ${settings}`}>
-  //               <Link to="" className="sidebar__links">
-  //                 <span>
-  //                   ثوابت النظام
-  //             {' '}
-  //                   {' '}
-  //                   <Icon type="setting"style={{marginLeft:'7px'}} />
-  //                 </span>
-
-  //               </Link>
-  //               <Link to="" className="sidebar__links">
-  //                 <span>
-  //                   ثوابت لوحة التحكم
-  //             {' '}
-  //                   {' '}
-  //                   <Icon type="control"style={{marginLeft:'7px'}} />
-  //                 </span>
-
-  //               </Link>
-  //               <Link to="" className="sidebar__links">
-  //                 <span>
-  //                   إدارة الاماكن العامة
-  //             {' '}
-  //                   {' '}
-  //                   <Icon type="environment"style={{marginLeft:'7px'}} />
-  //                 </span>
-
-  //               </Link>
-  //             </div>
-  //           <div className='home'>
-  //             <Link to="" className="sidebar__links">
-  //               <span>
-  //                 إتصل بنا
-  //           {' '}
-  //                 <Icon type="phone"style={{marginLeft:'5px'}} />
-  //               </span>
-
-  //             </Link>
-  //           </div>
-
-  //         </div>
-  //       </div>
-  //     </div>
-  //   );
-  // }
-
-render() {
-
-  const { accounts, reports, msg, settings,arrow_down_accounts,arrow_up_accounts ,arrow_down_reports,arrow_up_reports,arrow_down_msg,arrow_up_msg,arrow_down_settings,arrow_up_settings} = this.state;
-  return (
-    <div className="container">
-
-      <div className="sidebar">
-        <h1 className="sidebar__header">زاجل</h1>
-        <div className="sidebar__bar">
-          <div className='home'>
-            <Link to="/" className="sidebar__links ">
-              <span>
-                الرئيسية
-          <Icon type="home" style={{marginLeft:'7px'}}/>
-          </span>
-
-</Link>
-</div>
-<div className='home'>
-<Link to="/" className="sidebar__links" >
-  <span>
-    إدارة الطلبات
-{' '}
-    <Icon type="menu-unfold" style={{marginLeft:'5px'}}/>
-  </span>
-
-</Link>
-</div>
-
-
-<div className="sidebar__dropdown lists" onClick={this.handleClick('accounts')}>
-إدارة الحسابات
-{' '}
-
-<Icon type="tool" />
-{' '}
-<span className={`${arrow_down_accounts}`}>
-<Icon type="down" style={{marginLeft:'5px',fontSize:'15px'}}/>
-
-</span>
-            <span className={`${arrow_up_accounts}`}>
-            <Icon type="up" style={{marginLeft:'5px',fontSize:'15px'}} />
-            </span>
+              </Link>
             </div>
-            <div className={`sidebar__dropdowncontainer ${accounts}`}>
+            <div className='home'>
+              <Link to="/" className="sidebar__links" >
+                <span>
+                  إدارة الطلبات
+            {' '}
+                  <Icon type="menu-unfold" style={{marginLeft:'5px'}}/>
+                </span>
+
+              </Link>
+            </div>
+
+
+            <div className="sidebar__dropdown lists" onClick={this.handleClick('accounts')}>
+              إدارة الحسابات
+          {' '}
+
+              <Icon type="tool" />
+              {' '}
+              <Icon  type={accounts.arrow} style={{marginLeft:'5px',fontSize:'15px'}}/>
+            </div>
+            <div className={`sidebar__dropdowncontainer ${accounts.display}`}>
                 <Link to="/customers" className="sidebar__links ">
                   <span>
                     المستخدمين
@@ -322,19 +107,13 @@ render() {
               </div>
 
             <div onClick={this.handleClick('reports')} className="sidebar__dropdown lists" >
-
+           
               إدارة التقارير
           {' '}
               <Icon type="profile" />
-              <span className={`${arrow_down_reports}`}>
-            <Icon type="down" style={{marginLeft:'7px',fontSize:'15px'}}/>
-            </span>
-            <span className={`${arrow_up_reports}`}>
-            <Icon type="up" style={{marginLeft:'7px',fontSize:'15px'}} />
-            </span>
-
+              <Icon  type={accounts.arrow} style={{marginLeft:'7px',fontSize:'15px'}}/>
             </div>
-            <div className={`sidebar__dropdowncontainer ${reports}`}>
+            <div className={`sidebar__dropdowncontainer ${reports.display}`}>
                 <Link to="" className="sidebar__links ">
                   <span>
                     تقارير الكابتن
@@ -360,17 +139,12 @@ render() {
               </div>
 
             <div onClick={this.handleClick('msg')} className="sidebar__dropdown lists" >
-
+           
               إدارة الرسائل
           <Icon type="mail" style={{marginLeft:'7px'}}/>
-          <span className={`${arrow_down_msg}`}>
-            <Icon type="down" style={{marginLeft:'7px',fontSize:'15px'}}/>
-            </span>
-            <span className={`${arrow_up_msg}`}>
-            <Icon type="up" style={{marginLeft:'7px',fontSize:'15px'}} />
-            </span>
+          <Icon  type={accounts.arrow} style={{marginLeft:'5px',fontSize:'15px'}}/>
             </div>
-            <div className={`sidebar__dropdowncontainer ${msg}`}>
+            <div className={`sidebar__dropdowncontainer ${msg.display}`}>
                 <Link to="" className="sidebar__links ">
                   <span>
                     رسائل قصيرة
@@ -412,18 +186,13 @@ render() {
 
 
             <div onClick={this.handleClick('settings')} className="sidebar__dropdown lists" >
-
+            
               الإعدادات
           {' '}
-              <Icon type="setting" style={{marginLeft:'5px'}}/>
-              <span className={`${arrow_down_settings}`}>
-            <Icon type="down" style={{marginLeft:'7px',fontSize:'15px'}}/>
-            </span>
-            <span className={`${arrow_up_settings}`}>
-            <Icon type="up" style={{marginLeft:'7px',fontSize:'15px'}} />
-            </span>
+              <Icon type="setting" style={{marginLeft:'7px'}}/>
+              <Icon  type={accounts.arrow} style={{marginLeft:'5px',fontSize:'15px'}}/>
             </div>
-            <div className={`sidebar__dropdowncontainer ${settings}`}>
+            <div className={`sidebar__dropdowncontainer ${settings.display}`}>
                 <Link to="" className="sidebar__links">
                   <span>
                     ثوابت النظام
@@ -468,9 +237,4 @@ render() {
       </div>
     );
   }
-
 }
-
-
-
-
