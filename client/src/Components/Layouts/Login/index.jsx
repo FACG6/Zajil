f54867b661acef4019a7884fca637671ac9e2145
shorouq <shrooqabdullahsaad@gmail.com>
2019-06-01 22:21:03 +0300

    modify the calss name of input

--- client/src/Components/Layouts/Login/index.jsx
+++ client/src/Components/Layouts/Login/index.jsx
@@ -37,8 +37,8 @@ export default class Login extends Component {
         <h2 className='login__title'>زاجل</h2>
         <div className='login__box'>
         <h3 className="login__box__title">تسجيل الدخول</h3>
-          <input type="text" name="userName" id="userName" placeholder="إسم المستخدم" onChange={this.handleChange} className='login__box-input' />
-          <input type="password" name="password" id="password" placeholder="كلمة المرور" onChange={this.handleChange} className='login__box-input' />
+          <input type="text" name="userName" id="userName" placeholder="إسم المستخدم" onChange={this.handleChange} className='login__box__input' />
+          <input type="password" name="password" id="password" placeholder="كلمة المرور" onChange={this.handleChange} className='login__box__input' />
           <input type="submit" value="تسجيل الدخول" onClick={this.handleClick} className='login__box-button' />
         </div>
       </div>
