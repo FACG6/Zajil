9f74f197666da9f9bdfe7f59491a4f5ea5d25539
shorouq <shrooqabdullahsaad@gmail.com>
2019-06-02 10:51:36 +0300

    create initial class

---
+++ client/src/Components/Layouts/SingleCustomer/index.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+    state = {  }
+    render() { 
+        return (  <p>done</p>);
+    }
+}
+ 
+export default Profile;
