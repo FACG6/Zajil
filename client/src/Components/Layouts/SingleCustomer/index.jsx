1d2d3b5a3812e58b493e6fe5725666a329539c78
shorouq <shrooqabdullahsaad@gmail.com>
2019-06-02 13:40:32 +0300

    create the structure of personal information

--- client/src/Components/Layouts/SingleCustomer/index.jsx
+++ client/src/Components/Layouts/SingleCustomer/index.jsx
@@ -5,53 +5,60 @@ import Sidebar from "../../CommonComponent/Sidebar";
 import Navbar from "../../CommonComponent/Navbar";
 import Header from "../../CommonComponent/Header";
 
+import './style.css';
+
 class Profile extends Component {
   state = {
-      personalInformation: {
-          name: '',
-          phone: '',
-          status: '',
-          email: '',
-          address: '',
-          licence: '',
-          ID: ''
-      }
+    personalInformation: {
+      name: "شروق عبدالله سعد",
+      phone: "0599999999",
+      status: "فعال",
+      email: "shrooqabdullahsaad@gmail.com",
+      address: "غزة",
+      licence: "059999999999",
+      ID: "00000000"
+    }
   };
   render() {
+    const {
+      personalInformation: { name, phone, status, email, address, licence, ID }
+    } = this.state;
     return (
       <>
         <Navbar />
         <Sidebar />
         <Header Icon={<Icon type="user" />} title="الصفحة الشخصية" />
         <div className="profile">
-          <div className="profile__box">
-            <p className="profile__name">الاسم</p>
-            <p className="profile__value"></p>
-          </div>
-          <div className="profile__box">
-            <p className="profile__phone">الهاتف المحمول</p>
-            <p className="profile__value"></p>
-          </div>
-          <div className="profile__box">
-            <p className="profile__status">الحالة</p>
-            <p className="profile__value"></p>
-          </div>
-          <div className="profile__box">
-            <p className="profile__email">البريد الالكتروني</p>
-            <p className="profile__value"></p>
-          </div>
-          <div className="profile__box">
-            <p className="profile__address">العنوان</p>
-            <p className="profile__value"></p>
-          </div>
+          <div className="profile__info">
+            <div className="profile__box">
+              <p className="profile__box__title">الاسم</p>
+              <p className="profile__value">{name}</p>
+            </div>
+            <div className="profile__box">
+              <p className="profile__box__title">الهاتف المحمول</p>
+              <p className="profile__value">{phone}</p>
+            </div>
+            <div className="profile__box">
+              <p className="profile__box__title">الحالة</p>
+              <p className="profile__value">{status}</p>
+            </div>
+            <div className="profile__box">
+              <p className="profile__box__title">البريد الالكتروني</p>
+              <p className="profile__value">{email}</p>
+            </div>
+            <div className="profile__box">
+              <p className="profile__box__title">العنوان</p>
+              <p className="profile__value">{address}</p>
+            </div>
 
-          <div className="profile__box">
-            <p className="profile__licence">رقم الرخصة</p>
-            <p className="profile__value"></p>
-          </div>
-          <div className="profile__box">
-            <p className="profile__id">رقم الهوية</p>
-            <p className="profile__value"></p>
+            <div className="profile__box">
+              <p className="profile__box__title">رقم الرخصة</p>
+              <p className="profile__value">{licence}</p>
+            </div>
+            <div className="profile__box">
+              <p className="profile__box__title">رقم الهوية</p>
+              <p className="profile__value">{ID}</p>
+            </div>
           </div>
         </div>
       </>
