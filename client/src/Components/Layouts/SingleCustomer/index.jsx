d0eb6a640d3c87b7db7a7460f9e8c350b59e122c
shorouq <shrooqabdullahsaad@gmail.com>
2019-06-02 12:31:49 +0300

    initial structure for personal information

--- client/src/Components/Layouts/SingleCustomer/index.jsx
+++ client/src/Components/Layouts/SingleCustomer/index.jsx
@@ -1,10 +1,62 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Icon } from "antd";
+
+import Sidebar from "../../CommonComponent/Sidebar";
+import Navbar from "../../CommonComponent/Navbar";
+import Header from "../../CommonComponent/Header";
 
 class Profile extends Component {
-    state = {  }
-    render() { 
-        return (  <p>done</p>);
-    }
+  state = {
+      personalInformation: {
+          name: '',
+          phone: '',
+          status: '',
+          email: '',
+          address: '',
+          licence: '',
+          ID: ''
+      }
+  };
+  render() {
+    return (
+      <>
+        <Navbar />
+        <Sidebar />
+        <Header Icon={<Icon type="user" />} title="الصفحة الشخصية" />
+        <div className="profile">
+          <div className="profile__box">
+            <p className="profile__name">الاسم</p>
+            <p className="profile__value"></p>
+          </div>
+          <div className="profile__box">
+            <p className="profile__phone">الهاتف المحمول</p>
+            <p className="profile__value"></p>
+          </div>
+          <div className="profile__box">
+            <p className="profile__status">الحالة</p>
+            <p className="profile__value"></p>
+          </div>
+          <div className="profile__box">
+            <p className="profile__email">البريد الالكتروني</p>
+            <p className="profile__value"></p>
+          </div>
+          <div className="profile__box">
+            <p className="profile__address">العنوان</p>
+            <p className="profile__value"></p>
+          </div>
+
+          <div className="profile__box">
+            <p className="profile__licence">رقم الرخصة</p>
+            <p className="profile__value"></p>
+          </div>
+          <div className="profile__box">
+            <p className="profile__id">رقم الهوية</p>
+            <p className="profile__value"></p>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
- 
+
 export default Profile;
