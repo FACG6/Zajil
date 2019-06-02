4936dfe90e6dde70066b084e2811f584adf48801
shorouq <shrooqabdullahsaad@gmail.com>
2019-06-02 09:44:01 +0300

    add login route to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,22 @@
-import React, {Component} from 'react';
-import {BrowserRouter,Route} from 'react-router-dom';
-import { viewPopup, editPopup, deletePopup } from './Components/CommonComponent/table/Popups';
-import TableComponent from './Components/CommonComponent/table/Table';
-import SideBar from './Components/CommonComponent/Sidebar';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Login from './Components/Layouts/Login';
 import './App.css';
 
-import Home from './Components/Layouts/Home';
-
 class App extends Component {
-  state = {};
+  state = {
+    
+  }
   render() {
     return (
       <div className="App">
-        <Home />
-      <TableComponent
-        pageName="customers"
-        columns={[{
-          key: 54, customer: 'fgd', captain: 'fgdf', date: 'dfsdf',
-        }, {
-          key: 56, customer: 'fgd', captain: 'fgdf', date: 'dfsdf',
-        }]}
-        viewPopup={viewPopup}
-        editPopup={editPopup}
-        deletePopup={deletePopup}
-      />
-    </div>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/login" component={Login} />
+          </Switch>
+        </BrowserRouter>
+      </div>
     );
   }
 }
