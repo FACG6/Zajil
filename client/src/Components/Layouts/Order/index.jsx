9a1daaa2f345f0c92b31fc1f63583f0251b0c315
denaSH <denamoean17@gmail.com>
2019-06-02 14:38:53 +0300

    button to test popup add order

---
+++ client/src/Components/Layouts/Order/index.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import AddOrder from './addOrder'
+
+class Order extends Component{
+  render(){
+      return(
+        <div>
+         <AddOrder />
+        </div>
+);
+}
+}
+
+export default Order;
