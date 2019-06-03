e88d5ef929a50a8cc2fe98edcda1a270356be1bc
shorouq <shrooqabdullahsaad@gmail.com>
2019-06-03 03:36:53 +0300

    add fake data for customers informations

--- server/database/config/dbFake.sql
+++ server/database/config/dbFake.sql
@@ -1,5 +1,18 @@
 BEGIN;
 
-INSERT INTO users(user_name, password, role) VALUES('admin', '$2a$05$WRLE5TPY4ppszWklRiX0m.kjyX8uMtZ76O9lt8y8zXilM.vl/5hjS', 'admin');
+    INSERT INTO users
+        (user_name, password, role)
+    VALUES('admin', '$2a$05$WRLE5TPY4ppszWklRiX0m.kjyX8uMtZ76O9lt8y8zXilM.vl/5hjS', 'admin');
+-- for customer information
+    INSERT INTO TUser
+        (s_name, s_mobile_number, s_email, b_status, s_address, s_image, i_type, s_password)
+    VALUES
+        ('نور عماد نور', '+970599999999', 'test@hotmail.com', true, 'gaza', 'test.png', 1, '123'),
+        ('أنور عماد نور', '+970599999999', 'test@hotmail.com', true, 'khanyouns', 'test.png', 1, '123'),
+        ('منور عماد نور', '+970599999999', 'test@hotmail.com', true, 'rafah', 'test.png', 1, '123'),
+        ('فنور عماد نور', '+970599999999', 'test@hotmail.com', false, 'gaza', 'test.png', 1, '123'),
+        ('سنور عماد نور', '+970599999999', 'test@hotmail.com', true, 'dear albalah', 'test.png', 1, '123');
+    
+    
 
-COMMIT;
+    COMMIT;
