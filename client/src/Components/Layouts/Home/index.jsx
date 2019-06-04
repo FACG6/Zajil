6630e2d14d27c057c1b7a0329ac4e9959c99846a
shorouq <shrooqabdullahsaad@gmail.com>
2019-06-04 13:04:23 +0300

    transform from stateless to statfull

--- client/src/Components/Layouts/Home/index.jsx
+++ client/src/Components/Layouts/Home/index.jsx
@@ -1,23 +1,50 @@
-import React from 'react';
-import { Icon } from 'antd';
+import React, { Component } from "react";
 
-import Navbar from '../../CommonComponent/Navbar';
-import CountBox from '../../CommonComponent/CountBox';
-import Header from '../../CommonComponent/Header';
+import { Icon } from "antd";
 
-import './style.css';
+import Navbar from "../../CommonComponent/Navbar";
+import CountBox from "../../CommonComponent/CountBox";
+import Header from "../../CommonComponent/Header";
 
-const Home = () => (
-  <div>
-    <Navbar />
-    <Header title="الرئيسية" Icon={<Icon type="bank" />} />
+import "./style.css";
+
+class Home extends Component {
+  state = {
     
+  }
+  render() {
     <div>
-        <div className="box1"><CountBox title="الزبائن" number="10" color="#156FE9" icon={<Icon type="team" />} /></div>
-        <div className="box-two"><CountBox title="الكباتن" number="15" color="#F86363" icon={<Icon type="car" />} /></div>
-        <div className="box-three"><CountBox title="الطلبات" number="40" color="#55B690" icon={<Icon type="container" />} /></div>
-    </div>
-    </div>
-);
+      <Navbar />
+      <Header title="الرئيسية" Icon={<Icon type="bank" />} />
+
+      <div>
+        <div className="box1">
+          <CountBox
+            title="الزبائن"
+            number="10"
+            color="#156FE9"
+            icon={<Icon type="team" />}
+          />
+        </div>
+        <div className="box-two">
+          <CountBox
+            title="الكباتن"
+            number="15"
+            color="#F86363"
+            icon={<Icon type="car" />}
+          />
+        </div>
+        <div className="box-three">
+          <CountBox
+            title="الطلبات"
+            number="40"
+            color="#55B690"
+            icon={<Icon type="container" />}
+          />
+        </div>
+      </div>
+    </div>;
+  }
+}
 
 export default Home;
