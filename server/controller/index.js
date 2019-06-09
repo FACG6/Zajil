f03b7ceb657e77bf971cd5687941b75e59714f0b
Jamalat <jamalatshamallakh@gmail.com>
2019-06-10 01:32:40 +0300

    send resulte as an object and add customer route in index.js in custmer file

--- server/controller/index.js
+++ server/controller/index.js
@@ -5,12 +5,11 @@ const express = require('express');
 const { checkAuth } = require('./middleware/authentication');
 const { protectRoutes } = require('./middleware/protectRoute');
 const adminHandler = require('./accountsMangment/admin');
-const { getCustomer } = require('./accountsMangment/customer/getCustomers');
 const customerHandler = require('./accountsMangment/customer');
 const orderHandler = require('./order');
 
 const router = express.Router();
-router.route('/customers').get(getCustomer);
+
 router.use(checkAuth);
 // the all routes start from here
 
@@ -18,7 +17,6 @@ router.use(adminHandler);
 
 router.use(protectRoutes);
 // the protected route start from here
-
 router.use(customerHandler);
 router.use(orderHandler);
 
