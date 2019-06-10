5ee726a1639d98167a13b7f4bbefb3de1a83b16a
Jamalat <jamalatshamallakh@gmail.com>
2019-06-10 10:26:28 +0300

    solve conflict

--- server/controller/index.js
+++ server/controller/index.js
@@ -10,10 +10,6 @@ const customerHandler = require('./accountsMangment/customer');
 const orderHandler = require('./order');
 
 const router = express.Router();
-<<<<<<< HEAD
-=======
-
->>>>>>> 530de03af1830575aab78839bd7bdea5c8cd9877
 router.use(checkAuth);
 // the all routes start from here
 
