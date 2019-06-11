312f7721435532dcefe20787a244af9540acf584
shorouq <shrooqabdullahsaad@gmail.com>
2019-06-11 10:24:22 +0300

    add getCustomerOrders route in index of customer handler

--- server/controller/accountsMangment/customer/index.js
+++ server/controller/accountsMangment/customer/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 
 const { getDetails } = require('./getCustomerDetails');
+const { getOrdersDetails } = require('./getCustomerOrders');
 
 const router = express.Router();
 
 router.route('/getCustomerDetails/:id')
   .get(getDetails);
+
+router.route('/getCustomerOrders/:id')
+  .get(getOrdersDetails);
 
 module.exports = router;
