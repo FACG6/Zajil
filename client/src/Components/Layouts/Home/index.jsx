c8b72a9a8a960a99246b08fdef473dbd4277003e
Anies <a2nies2012@gmail.com>
2019-06-11 20:44:23 +0300

    add display flex, flex direction to the navbar and header components

--- client/src/Components/Layouts/Home/index.jsx
+++ client/src/Components/Layouts/Home/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Icon } from 'antd';
 
 import Navbar from '../../CommonComponent/Navbar';
+import Sidebar from '../../CommonComponent/Sidebar/index';
 import CountBox from '../../CommonComponent/CountBox';
 import Header from '../../CommonComponent/Header';
 
@@ -9,15 +10,39 @@ import './style.css';
 
 const Home = () => (
   <div>
-    <Navbar />
-    <Header title="الرئيسية" Icon={<Icon type="bank" />} />
-    
-    <div>
-        <div className="box1"><CountBox title="الزبائن" number="10" color="#156FE9" icon={<Icon type="team" />} /></div>
-        <div className="box-two"><CountBox title="الكباتن" number="15" color="#F86363" icon={<Icon type="car" />} /></div>
-        <div className="box-three"><CountBox title="الطلبات" number="40" color="#55B690" icon={<Icon type="container" />} /></div>
+    <Sidebar />
+    <div className="Home_navbar-and-header-container">
+      <Navbar />
+      <Header title="الرئيسية" Icon={<Icon type="bank" />} />
+
+      <div>
+        <div className="box1">
+          <CountBox
+            title="الزبائن"
+            number="10"
+            color="#156FE9"
+            icon={<Icon type="team" />}
+          />
+        </div>
+        <div className="box-two">
+          <CountBox
+            title="الكباتن"
+            number="15"
+            color="#F86363"
+            icon={<Icon type="car" />}
+          />
+        </div>
+        <div className="box-three">
+          <CountBox
+            title="الطلبات"
+            number="40"
+            color="#55B690"
+            icon={<Icon type="container" />}
+          />
+        </div>
+      </div>
     </div>
-    </div>
+  </div>
 );
 
 export default Home;
