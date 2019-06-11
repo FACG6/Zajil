fdf553186fe091e3b4c2ed3d744326728158949a
Jamalat <jamalatshamallakh@gmail.com>
2019-06-12 01:57:14 +0300

    create CollectionCreateForm component Relates #158

---
+++ client/src/Components/Layouts/Customers/addcustomer.jsx
@@ -0,0 +1,19 @@
+import antd, { Icon } from 'antd';
+import React, { Component } from 'react'
+import validator from 'validator'
+import './style.css'
+
+const { Modal, Form, Input, Select } = antd;
+const { Option } = Select;
+const CollectionCreateForm = Form.create({ name: 'form_in_modal' })(
+  // eslint-disable-next-line
+
+  class extends React.Component {
+
+   render(){
+     return()
+   }
+  
+  },
+)
+export default CollectionCreateForm;
