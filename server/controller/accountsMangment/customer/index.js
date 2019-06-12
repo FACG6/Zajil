7c09f053e4a6fedbadf77223f4d672aff65627b2
Jamalat <jamalatshamallakh@gmail.com>
2019-06-12 06:42:41 +0300

    add addcustomer route

--- server/controller/accountsMangment/customer/index.js
+++ server/controller/accountsMangment/customer/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const { getDetails } = require('./getCustomerDetails');
 const { getCustomer } = require('./getCustomers');
+const { addNewCustomer } = require('./addCustomer');
 
 const router = express.Router();
 
 router.route('/getCustomerDetails/:id')
   .get(getDetails);
 router.route('/customers').get(getCustomer);
+router.route('/addcustomer').post(addNewCustomer);
 module.exports = router;
