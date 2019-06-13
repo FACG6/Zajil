d776682c77f2d01ec7c4cec6f833d0d174e4a97f
shorouq <shrooqabdullahsaad@gmail.com>
2019-06-13 14:06:38 +0300

    puth the inforamtion of order in modal

--- client/src/Components/Layouts/SingleCustomer/Popups/viewPopUp.jsx
+++ client/src/Components/Layouts/SingleCustomer/Popups/viewPopUp.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Modal, notification } from "antd";
+import { Modal, Table} from "antd";
 
 class View extends Component {
   state = {
@@ -8,27 +8,8 @@ class View extends Component {
     information: null
   };
 
-//   handleOk = e => {
-//     fetch(`/api/v1/deleteOrder/${this.props.id}`, { method: "DELETE" })
-//       .then(res => res.json())
-//       .then(res => {
-//         const { error } = res;
-//         if (error) {
-//           this.openNotificationWithIcon("error", error);
-//         } else {
-//           this.props.updateState(this.state.id);
-//           this.openNotificationWithIcon("success", "Delete Done");
-//         }
-//         this.props.visibleFun("singleCustomer", "deleteVisibility")(e);
-//       })
-//       .catch(() => {
-//         this.openNotificationWithIcon('warning', 'Something error please try again');
-//         this.props.visibleFun("singleCustomer", "deleteVisibility")(e);
-//       });
-//   };
-
   handleCancel = e => {
-    this.props.visibleFun("singleCustomer", "deleteVisibility")(e);
+    this.props.visibleFun("singleCustomer", "viewVisibility")(e);
   };
   componentWillReceiveProps(props) {
     const { visible, id, information } = props;
@@ -37,21 +18,56 @@ class View extends Component {
   }
 
   render() {
-    return (
-      <Modal
-        title="عرض الطلب"
-        visible={this.state.visible}
-        onOk={this.handleOk}
-        cancelText="اغلاق"
-        onCancel={this.handleCancel}
-        closable={false}
-        style={{ direction: "rtl" }}
-        className="viewModal"
-      >
-        <p>view</p>
-      </Modal>
-    );
+      const columns = [{title: 'اسم الطلبية', dataIndex: 'f1'},
+      {title: 'السعر', dataIndex: 'f2'}]
+    if (this.state.information) {
+      const { information } = this.state;
+      return (
+        <Modal
+          title="عرض الطلب"
+          visible={this.state.visible}
+          cancelText="اغلاق"
+          onCancel={this.handleCancel}
+          closable={false}
+          style={{ direction: "rtl", width: '575' }}
+          className="viewModal"
+        >
+          <div className="view__captain">
+            <div className="view__captain-box">
+              <p>اسم الكابتن : </p>
+              <p className="view__captain-value">{information.captain}</p>
+            </div>
+            <div className="view__captain-box">
+              <p>تاريخ الطلبية : </p>
+              <p className="view__captain-value">{information.date}</p>
+            </div>
+            <div className="view__captain-box">
+              <p>اسم المكان : </p>
+              <p className="view__captain-value">{information.place}</p>
+            </div>
+            <div className="view__captain-box">
+              <p>حالة الطلب : </p>
+              <p className="view__captain-value">{information.status}</p>
+            </div>
+            <Table dataSource={information.items} columns={columns} className="view__captain-table"/>
+            <div className="view__captain-box">
+              <p>السعر الكلي : </p>
+              <p className="view__captain-value">{information.price}</p>
+            </div>
+          </div>
+        </Modal>
+      );
+    }
+    return "";
   }
 }
+
+// captain: "احمد محمد محمود"
+// date: "2019-06-11"
+// items: ["بندورة"]
+// key: "3"
+// place: undefined
+// price: "20$"
+// status: "تم"
 
 export default View;
