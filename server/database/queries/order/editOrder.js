4816cefe59cef51e1d10ca796045356de1830a05
Anies <a2nies2012@gmail.com>
2019-06-13 13:09:00 +0300

    remove the returning keyword from the query

--- server/database/queries/order/editOrder.js
+++ server/database/queries/order/editOrder.js
@@ -4,14 +4,14 @@ const editOrders = (phone, address, item, date, placeId, id) => {
   const values1 = [phone, address, placeId, date, id];
   return connection
     .query(
-      'UPDATE orders SET s_customer_phone = $1, s_customer_address = $2, fk_i_place_id = $3, dt_modified_date = $4 FROM places WHERE orders.pk_i_id = $5 RETURNING orders.pk_i_id',
+      'UPDATE orders SET s_customer_phone = $1, s_customer_address = $2, fk_i_place_id = $3, dt_modified_date = $4 FROM places WHERE orders.pk_i_id = $5',
       values1,
     )
     .then(() => {
       const values2 = [item, date, id];
       connection
         .query(
-          'UPDATE items SET s_name = $1, dt_modified_date = $2 WHERE  fk_i_order_id = $3 RETURNING items.s_name',
+          'UPDATE items SET s_name = $1, dt_modified_date = $2 WHERE  fk_i_order_id = $3',
           values2,
         )
         .catch(err => ({ error: err.message }));
