f94a28a465acb4fcfd4c3ba3953675b3458720e1
Jamalat <jamalatshamallakh@gmail.com>
2019-06-13 13:12:57 +0300

    add loading component

--- client/src/Components/Layouts/Customers/index.jsx
+++ client/src/Components/Layouts/Customers/index.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { css } from '@emotion/core';
+import { ClipLoader } from 'react-spinners';
 import './style.css';
 import { Input, notification, Icon, DatePicker } from 'antd';
 import Table from '../../CommonComponent/Table/Table';
@@ -9,6 +11,11 @@ import Navbar from "../../CommonComponent/Navbar/index";
 import CollectionCreateForm from "./addcustomer"
 
 const { RangePicker } = DatePicker;
+const override = css`
+    display: block;
+    margin: 0 auto;
+    border-color: red;
+`;
 
 export default class Customers extends Component {
     state = {
@@ -20,6 +27,7 @@ export default class Customers extends Component {
         filteredcustomersDate: [],
         filteredcustomersName: [],
         visible: false,
+        loading: true
     }
     componentDidMount() {
         fetch('api/v1/customers').then(res => res.json())
@@ -178,6 +186,18 @@ export default class Customers extends Component {
                     </div>
                 </div>
             )
+        }else{
+            return (
+                <div className='sweet-loading'>
+                  <ClipLoader
+                    css={override}
+                    sizeUnit={"px"}
+                    size={150}
+                    color={'#123abc'}
+
+                    loading={this.state.loading}
+                  />
+                </div>)
         }
     }
 }
