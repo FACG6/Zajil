8826083fd4154a27074395c9bc7fc9441da0d169
Nareman <naremanmohhilles@gmail.com>
2019-06-17 15:26:22 +0300

    edit style of label form

--- client/src/Components/Layouts/Captains/EditCaptain/editCaptain.jsx
+++ client/src/Components/Layouts/Captains/EditCaptain/editCaptain.jsx
@@ -20,7 +20,8 @@ const CollectionCreateForm = Form.create({ name: "form_in_modal" })(
       this.setState({ imageUrl: e.file });
     };
     componentDidMount() {
-      const { id } = this.props.id;
+      const id = 6;
+      // const { id } = this.props.id;
       fetch(`/api/v1/getCaptainDetails/${id}`)
         .then(res => res.json())
         .then(res => {
@@ -236,8 +237,8 @@ class CollectionsPage extends React.Component {
         formData.append('password', password);
         formData.append('phone', phone);
         formData.append('status', status);
-
-        const { id } = this.props.id;
+        const id = 6;
+        // const { id } = this.props.id;
         fetch(`/api/v1/putCaptain/${id}`, {
           method: 'PUT',
           body: formData,
