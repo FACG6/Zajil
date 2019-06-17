26be9aee9541d472affdd245153f34b7b83ebe4e
Anies <a2nies2012@gmail.com>
2019-06-17 14:39:15 +0300

    handle error

--- client/src/Components/CommonComponent/Table/Popups.jsx
+++ client/src/Components/CommonComponent/Table/Popups.jsx
@@ -20,10 +20,15 @@ class EditForm extends React.Component {
 
   componentDidMount() {
     this.setState({
-      itemsInputs: this.props.itemsArray ?  JSON.parse(JSON.stringify(this.props.itemsArray)) : [],
-      originalItems: this.props.itemsArray ? JSON.parse(JSON.stringify(this.props.itemsArray)) : []
+      itemsInputs: this.props.itemsArray
+        ? JSON.parse(JSON.stringify(this.props.itemsArray))
+        : [],
+      originalItems: this.props.itemsArray
+        ? JSON.parse(JSON.stringify(this.props.itemsArray))
+        : []
     });
   }
+
   handleSubmit = e => {
     e.preventDefault();
     this.props.form.validateFields(async (error, values) => {
@@ -32,158 +37,162 @@ class EditForm extends React.Component {
       for (let i = 0; i < this.props.stores.length; i++) {
         if (this.props.stores[i].value == values.storeName) {
           storeId = this.props.stores[i].id;
-          for (let i = 0; i < this.props.stores.length; i++) {
-            if (this.props.stores[i].value == values.storeName) {
-              storeId = this.props.stores[i].id;
-            }
-          }
-          let deletedItems = [],
-          newItems = [];
-        for (let i =0; i < this.state.originalItems.length; i++) {
-          let exist = false;
-          for (let j = 0; j < this.state.itemsInputs.length; j++) {
-            if (JSON.stringify(this.state.originalItems[i]) === JSON.stringify(this.state.itemsInputs[j])) {
-              exist = true;
-            }
-          }
-          if (!exist) {
-            deletedItems.push(this.state.originalItems[i]);
-          }
         }
-        for (let j in this.state.itemsInputs) {
-          let edited = true;
-          for (let i in this.state.originalItems) {
-            if (JSON.stringify(this.state.originalItems[i]) === JSON.stringify(this.state.itemsInputs[j])) {
-              edited = false;
-            }
-          }
-          if (edited) {
-            newItems.push(this.state.itemsInputs[j]);
-          }
-        }
-        if (!error) {
-          axios
-            .put(`/api/v1/editOrder/${this.props.orderId}`, {
-              phone:
-                document.querySelector(
-                  ".popupModal .ant-select-selection-selected-value"
-                ).title +
-                "-" +
-                values.phone,
-              address: values.address,
-              items: { deleted:deletedItems, edited:newItems },
-              storeID: storeId
-            })
-            .then(res => {
-              if (res.status == 200) {
-                this.props.form.resetFields();
-                this.setState({ visible: false });
-              } else {
-                this.setState({ error: "Try again please" });
-              }
-            })
-            .catch(err => {
-              this.setState({
-                error: err
-              });
-            });
-        }
-      });
-        }
-        showModal = () => {
-          this.setState({
-            visible: true,
-            error: ""
-          });
-        };
-      
-        handleCancel = () => {
-          this.props.form.resetFields();
-          this.setState({
-            visible: false,
-            itemsInputs: this.state.originalItems
-          });
-        };
-      
-        storeNameInput = async e => {
-          await this.setState({
-            storeNameManual: e.target.value
-          });
-        };
-      
-        validateStoreName = async (rule, value, callback) => {
-          await this.setState({ storeNameArray: value });
-          const { storeNameArray, storeNameManual } = this.state;
+      }
+      let deletedItems = [],
+        newItems = [];
+      for (let i = 0; i < this.state.originalItems.length; i++) {
+        let exist = false;
+        for (let j = 0; j < this.state.itemsInputs.length; j++) {
           if (
-            (storeNameArray && storeNameArray.length >= 1) ||
-            (storeNameManual && storeNameManual.length >= 3)
+            JSON.stringify(this.state.originalItems[i]) ===
+            JSON.stringify(this.state.itemsInputs[j])
           ) {
-            callback();
-          } else {
-            callback("يرجى إدخال المتجر !");
+            exist = true;
           }
-        };
-      
-        validateStoreNameManual = (rule, value, callback) => {
-          const { storeNameArray, storeNameManual } = this.state;
-          if (
-            (storeNameArray && storeNameArray.length >= 1) ||
-            (storeNameManual && storeNameManual.length >= 3)
-          ) {
-            callback();
-          } else {
-            callback("يرجى إدخال المتجر !");
-          }
-        };
-        validateItem = (rule, value, callback) => {
-          if (value && value.length >= 3) {
-            callback();
-          } else {
-            callback("يرجى إدخال عنصر !");
-          }
-        };
-        validateItemPrice = (rule, value, callback) => {
-          if (value && value.length >= 1) {
-            callback();
-          } else {
-            callback("يرجى إدخال السعر !");
-          }
-        };
-        appendInput = () => {
-          this.setState({
-            itemsInputs: this.state.itemsInputs.concat([{ name: "", price: "" }])
-          });
-        };
-        removeInput = index => {
-          let stateItems = this.state.itemsInputs;
-          stateItems.splice(index, 1);
-          this.props.form.resetFields();
-          this.setState({
-            itemsInputs: stateItems
-          });
-        };
-        setNewItem = async (key, val, index) => {
-          let newItem = [...this.state.itemsInputs];
-          if (key == "name") {
-            newItem[index].name = val.target.value;
-            this.setState({
-              itemsInputs: newItem
-            });
-          } else if (key == "price") {
-            newItem[index].price = val.target.value;
-            this.setState({
-              itemsInputs: newItem
-            });
-          }
-        };
-        resetEverything = () => {
-          this.props.form.resetFields();
-          this.setState({itemsInputs: this.state.originalItems})
         }
+        if (!exist) {
+          deletedItems.push(this.state.originalItems[i]);
+        }
+      }
+      for (let j in this.state.itemsInputs) {
+        let edited = true;
+        for (let i in this.state.originalItems) {
+          if (
+            JSON.stringify(this.state.originalItems[i]) ===
+            JSON.stringify(this.state.itemsInputs[j])
+          ) {
+            edited = false;
+          }
+        }
+        if (edited) {
+          newItems.push(this.state.itemsInputs[j]);
+        }
+      }
+      if (!error) {
+        axios
+          .put(`/api/v1/editOrder/${this.props.orderId}`, {
+            phone:
+              document.querySelector(
+                ".popupModal .ant-select-selection-selected-value"
+              ).title +
+              "-" +
+              values.phone,
+            address: values.address,
+            items: { deleted: deletedItems, edited: newItems },
+            storeID: storeId
+          })
+          .then(res => {
+            if (res.status == 200) {
+              this.props.form.resetFields();
+              this.setState({ visible: false });
+            } else {
+              this.setState({ error: "Try again please" });
+            }
+          })
+          .catch(err => {
+            this.setState({
+              error: err
+            });
+          });
+      }
+    });
+  };
 
+  showModal = () => {
+    this.setState({
+      visible: true,
+      error: ""
+    });
+  };
+
+  handleCancel = () => {
+    this.props.form.resetFields();
+    this.setState({
+      visible: false,
+      itemsInputs: this.state.originalItems
+    });
+  };
+
+  storeNameInput = async e => {
+    await this.setState({
+      storeNameManual: e.target.value
+    });
+  };
+
+  validateStoreName = async (rule, value, callback) => {
+    await this.setState({ storeNameArray: value });
+    const { storeNameArray, storeNameManual } = this.state;
+    if (
+      (storeNameArray && storeNameArray.length >= 1) ||
+      (storeNameManual && storeNameManual.length >= 3)
+    ) {
+      callback();
+    } else {
+      callback("يرجى إدخال المتجر !");
+    }
+  };
+
+  validateStoreNameManual = (rule, value, callback) => {
+    const { storeNameArray, storeNameManual } = this.state;
+    if (
+      (storeNameArray && storeNameArray.length >= 1) ||
+      (storeNameManual && storeNameManual.length >= 3)
+    ) {
+      callback();
+    } else {
+      callback("يرجى إدخال المتجر !");
+    }
+  };
+  validateItem = (rule, value, callback) => {
+    if (value && value.length >= 3) {
+      callback();
+    } else {
+      callback("يرجى إدخال عنصر !");
+    }
+  };
+  validateItemPrice = (rule, value, callback) => {
+    if (value && value.length >= 1) {
+      callback();
+    } else {
+      callback("يرجى إدخال السعر !");
+    }
+  };
+  appendInput = () => {
+    this.setState({
+      itemsInputs: this.state.itemsInputs.concat([{ name: "", price: "" }])
+    });
+  };
+  removeInput = index => {
+    let stateItems = this.state.itemsInputs;
+    stateItems.splice(index, 1);
+    this.props.form.resetFields();
+    this.setState({
+      itemsInputs: stateItems
+    });
+  };
+  setNewItem = async (key, val, index) => {
+    let newItem = [...this.state.itemsInputs];
+    if (key == "name") {
+      newItem[index].name = val.target.value;
+      this.setState({
+        itemsInputs: newItem
+      });
+    } else if (key == "price") {
+      newItem[index].price = val.target.value;
+      this.setState({
+        itemsInputs: newItem
+      });
+    }
+  };
+  resetEverything = () => {
+    this.props.form.resetFields();
+    this.setState({ itemsInputs: this.state.originalItems });
+  };
   render() {
     const { customerName, phoneNumber, customerAddress, storeId } = this.props;
     const { getFieldDecorator } = this.props.form;
+
     const formItemLayout = {
       labelCol: {
         xs: { span: 24 },
@@ -326,7 +335,6 @@ class EditForm extends React.Component {
                       )}
                     </Form.Item>
                   </Form.Item>
-
                   <div className="popupModal_form-extra-items-container">
                     <div style={{ "margin-bottom": "24px" }}>
                       {this.state.itemsInputs.slice(1).map((field, index) => {
@@ -396,7 +404,7 @@ class EditForm extends React.Component {
                 </div>
               </div>
               <div className="marketAndButtonsDiv">
-              <Form.Item
+                <Form.Item
                   label={
                     <span>
                       <span className="popupModal_storeName-label">*</span> إختر
@@ -448,47 +456,58 @@ class EditForm extends React.Component {
           </Modal>
         </React.Fragment>
       );
+    } else {
+      return (
+        <React.Fragment>
+          <Icon
+            type="edit"
+            style={{
+              fontSize: "1.2rem",
+              color: "rgba(0, 0, 0, 0.65)"
+            }}
+            onClick={this.showModal}
+          />
+          <Modal
+            className="ErrorPopupModal popupModal"
+            visible={this.state.visible}
+            onOk={this.handleOk}
+            onCancel={this.handleCancel}
+            destroyOnClose={{ disabled: false }}
+          >
+            <div className="popupModal_error-class">
+              <h1>
+                {this.state.error.response
+                  ? this.state.error.response.status
+                  : "Error"}{" "}
+                {this.state.error.response
+                  ? this.state.error.response.data
+                  : "try again later"}{" "}
+              </h1>
+            </div>
+            {this.resetEverything}
+          </Modal>
+        </React.Fragment>
+      );
     }
   }
+}
 const EditPopup = Form.create()(EditForm);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const deletePopup = (id, DataToBeDisplayedObject, deletePopupHtmlString) => {
-  const span = document.createElement('span');
+  const span = document.createElement("span");
   const details = deletePopupHtmlString;
   span.innerHTML = `${details}`;
   swal.fire({
-    title: 'Are you sure?',
+    title: "Are you sure?",
     showCancelButton: true,
     showConfirmButton: true,
-    confirmButtonColor: '#ff4343',
-    confirmButtonText: 'حذف',
-    cancelButtonText: 'إغلاق',
-    cancelButtonColor: '#2b2a37',
+    confirmButtonColor: "#ff4343",
+    confirmButtonText: "حذف",
+    cancelButtonText: "إغلاق",
+    cancelButtonColor: "#2b2a37",
     closeOnConfirm: true,
     reverseButtons: true,
-    html: span,
+    html: span
   });
 };
 
