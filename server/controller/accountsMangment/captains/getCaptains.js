504affe7eee2638f398d733d31df03a30f0a1092
Jamalat <jamalatshamallakh@gmail.com>
2019-06-19 17:22:06 +0300

    handel get captains and send the result to fromt side

---
+++ server/controller/accountsMangment/captains/getCaptains.js
@@ -0,0 +1,15 @@
+const { getCaptains } = require('../../../database/queries/captain/getCaptains');
+
+const getCaptain = (request, response) => {
+  getCaptains()
+    .then((res) => {
+      if (res.rows) {
+        console.log(res.rows);
+        response.status(200).send({ result: res.rows });
+      }
+    })
+    .catch(() => {
+      response.status(500).send({ error: 'Internal Server Error' });
+    });
+};
+module.exports = { getCaptain };
