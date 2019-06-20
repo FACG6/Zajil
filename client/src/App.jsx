05d34d732ebd1583c58e118209cf75a575a97166
Jamalat <jamalatshamallakh@gmail.com>
2019-06-20 10:43:26 +0300

    solve conflict

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,9 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Customers from "./Components/Layouts/Customers/index";
 import Login from "./Components/Layouts/Login";
 import Home from "./Components/Layouts/Home";
-<<<<<<< HEAD
-import Customers  from "./Components/Layouts/Customers/index"
-=======
 import OrdersManagement from "./Components/Layouts/Ordersmanagement";
 import SingleCaptain from "./Components/Layouts/SingleCaptains";
 import SingleCastomer from "./Components/Layouts/SingleCustomer";
->>>>>>> 125b00ae44cbf8eb7d6948030c9caec7f8357a1c
 import Captains from "./Components/Layouts/Captains";
 import NotFound from "./Components/Layouts/NotFound";
 import InProgress from './Components/Layouts/ToBuildLater';
@@ -52,43 +48,15 @@ class App extends Component {
             <Route
               exact
               path="/customers/profile/:id"
-              render={() => (
-                <div>
-                  <Sidebar />
-                  <Navbar />
-                  <Profile />
-                </div>
-              )}
-              />
-              <Route exact path="/orders" render = {() => (
-                <div>
-                <Sidebar />
-                <Navbar />
-                <Order />
-              </div>
-              )} />
-          
-          <Route
-            exact
-            path="/customers"
-            render={() => (<Customers />)}
-          />
-
-           <Route
-            exact
-            path="/captains"
-            render={() => (<Captains />)}
-          />
+              component={SingleCastomer}
+            />
             <Route
               exact
               path="/in-progress"
               component={InProgress}
             />
-<<<<<<< HEAD
-=======
 
             <Route component={NotFound} />
->>>>>>> 125b00ae44cbf8eb7d6948030c9caec7f8357a1c
           </Switch>
         </Router>
       </div>
