630d7ee914618b8fdd9a7b05282c2bf42da95c1e
shorouq <shrooqabdullahsaad@gmail.com>
2019-06-21 22:00:30 +0300

    remove old password and handle depend it

--- server/controller/accountsMangment/customer/editCustomer.js
+++ server/controller/accountsMangment/customer/editCustomer.js
@@ -3,9 +3,6 @@ const {
   checkCustomers,
 } = require('../../../database/queries/customer/checkCustomer');
 const {
-  checkCustomerId,
-} = require('../../../database/queries/customer/checkCustomerId');
-const {
   editcustomer,
 } = require('../../../database/queries/customer/editCustomer');
 
@@ -18,54 +15,59 @@ const editCustomer = (req, res) => {
     status,
     address,
     newpassword,
-    oldpassword,
   } = req.body;
-  checkCustomerId(id)
-    .then((checkResult) => {
-      const userpassword = checkResult.rows[0].s_password;
-      bcrypt
-        .compare(oldpassword, checkResult.rows[0].s_password)
-        .then((resultcheckHash) => {
-          if (resultcheckHash) {
-            checkCustomers(email)
-              .then(({ rows }) => {
-                if (!rows.length || rows[0].pk_i_id === id) {
-                  if (newpassword) {
-                    const hashNewPassword = bcrypt.hashSync(newpassword, 5);
-                    editcustomer(
-                      id,
-                      name,
-                      email,
-                      phone,
-                      status,
-                      address,
-                      hashNewPassword,
-                    ).then(resultEdit => res.status(200).send({ result: resultEdit.rows }));
-                  } else {
-                    editcustomer(
-                      id,
-                      name,
-                      email,
-                      phone,
-                      status,
-                      address,
-                      userpassword,
-                    )
-                      .then((resultEdit) => {
-                        res.status(200).send({ result: resultEdit.rows });
-                      })
-                      .catch(() => res.status(500).send({ error: 'internal server error' }));
-                  }
-                } else {
-                  res.status(400).send({ error: 'هذا الايميل مستخدم مسبقا' });
-                }
-              })
-              .catch(() => res.status(500).send({ error: 'internal server error' }));
-          } else res.status(400).send({ error: 'كلمة المرور الحالية خاطئة' });
-        })
-        .catch(() => res.status(500).send({ error: 'internal server error' }));
+  checkCustomers(email)
+    .then(({
+      rows,
+    }) => {
+      if (!rows.length || rows[0].pk_i_id === id) {
+        if (newpassword) {
+          const hashNewPassword = bcrypt.hashSync(newpassword, 5);
+          editcustomer(
+            id,
+            name,
+            email,
+            phone,
+            status,
+            address,
+            hashNewPassword,
+          ).then(resultEdit => res.status(200).send({
+            result: resultEdit.rows,
+          }));
+        } else {
+          editcustomer(
+            id,
+            name,
+            email,
+            phone,
+            status,
+            address,
+            newpassword,
+          )
+            .then((resultEdit) => {
+              console.log(resultEdit.rows);
+              res.status(200).send({
+                result: resultEdit.rows,
+              });
+            })
+            .catch((error) => {
+              console.log(error);
+              res.status(500).send({
+                error: 'internal server error',
+              });
+            });
+        }
+      } else {
+        res.status(400).send({
+          error: 'هذا الايميل مستخدم مسبقا',
+        });
+      }
     })
-    .catch(() => res.status(500).send({ error: 'لا يمكن اتمام هذه العملية' }));
+    .catch(() => res.status(500).send({
+      error: 'internal server error',
+    }));
 };
 
-module.exports = { editCustomer };
+module.exports = {
+  editCustomer,
+};
