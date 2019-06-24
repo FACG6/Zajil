876432453aa65824421b5c91c243699125164ff8
shorouq <shrooqabdullahsaad@gmail.com>
2019-06-24 14:17:13 +0300

    handle empty response

--- server/controller/accountsMangment/captains/getCaptainDetails.js
+++ server/controller/accountsMangment/captains/getCaptainDetails.js
@@ -3,8 +3,10 @@ const { getDetails } = require('../../../database/queries/captain/getCaptainDeta
 exports.getDetails = (req, res) => {
   const { id } = req.params;
   getDetails(id)
-    .then(({ rows }) => {
-      res.send({ result: rows });
+    .then(({ rows, rowCount }) => {
+      if (!rowCount) {
+        res.status(404).send({ notFound: 'Not Found' });
+      } else res.send({ result: rows });
     })
     .catch(() => {
       res.status(500).send({ error: 'Internal Server Error' });
