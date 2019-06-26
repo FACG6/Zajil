994d7be70c18d5724b5444310f8606074f3ec467
Nareman <naremanmohhilles@gmail.com>
2019-06-26 15:00:45 +0300

    add fake data to our website

--- server/controller/accountsMangment/captains/addCaptain.js
+++ server/controller/accountsMangment/captains/addCaptain.js
@@ -1,21 +1,14 @@
 const path = require('path');
 const { hash } = require('bcryptjs');
+const Joi = require('@hapi/joi');
 
 const { insertCaptain } = require('../../../database/queries/captain/addCaptain');
-
-const validation = (incomingObject) => {
-  const {
-    name, email, phone, address, IDNumber, licenceNumber, status, password,
-  } = incomingObject;
-  if (![name, email, phone, address, IDNumber, licenceNumber, status, password].every(ele => ele)) return false;
-  if (!(/^([أ-يa-z]|\s)+$/.test(name) && /.+\@.+\..+/.test(email) && /^\+?[0-9]{10,12}$/.test(phone) && /^[0-9]{7}$/.test(licenceNumber))) return false;
-  if (!(/^[0-9]{9}$/.test(IDNumber) && (status === 'true' || status === 'false'))) return false;
-  return true;
-};
+const { schema } = require('../../utils/addCaptainSchema');
 
 const addCaptain = (req, res) => {
   const { file } = req.files;
-  if (!validation(req.body)) {
+  const { error: validationError } = Joi.validate(req.body, schema);
+  if (validationError !== null) {
     return res.status(400).send({ error: 'Bad Request' });
   }
   const {
@@ -34,8 +27,10 @@ const addCaptain = (req, res) => {
               .then((response) => {
                 res.send({ result: response.rows });
               })
-              .catch(() => {
-                res.status(500).send({ error: 'Internal Server Error' });
+              .catch((errorInsert) => {
+                if (errorInsert.constraint === 'tuser_s_email_key') {
+                  res.status(409).send({ error: 'الايميل مستخدم' });
+                } else { res.status(500).send({ error: 'Internal Server Error' }); }
               });
           }
         });
