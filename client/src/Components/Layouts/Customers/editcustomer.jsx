3de275c9434f9c8ad1b37f7997a7d897f0ee57d6
shorouq <shrooqabdullahsaad@gmail.com>
2019-06-27 09:40:02 +0300

    add regex for name in edit customer

--- client/src/Components/Layouts/Customers/editcustomer.jsx
+++ client/src/Components/Layouts/Customers/editcustomer.jsx
@@ -70,7 +70,7 @@ const EditCustomer = Form.create({ name: "form_in_modal" })(
       } else cb("يرجى ادخال البريد الالكتروني");
     };
     handelName = (rule, value, cb) => {
-      if (!value) {
+      if (!value && !(/^([أ-يa-zA-Z0-9]|\s)+$/.test(value))) {
         cb("يرجى ادخال الاسم");
       } else {
         cb();
