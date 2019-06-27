0370f4609576a857a7c4cf5f656b5c0f4fcc48e0
shorouq <shrooqabdullahsaad@gmail.com>
2019-06-27 09:54:26 +0300

    add to regex capital latter

--- server/controller/utils/addCaptainSchema.js
+++ server/controller/utils/addCaptainSchema.js
@@ -1,7 +1,7 @@
 const Joi = require('@hapi/joi');
 
 exports.schema = Joi.object().keys({
-  name: Joi.string().regex(/^([أ-يa-z0-9]|\s)*$/).required(),
+  name: Joi.string().regex(/^([أ-يa-zِA-Z0-9]|\s)*$/).required(),
   email: Joi.string().email().required(),
   phone: Joi.string().regex(/^\+?[0-9]{10,12}$/).required(),
   licenceNumber: Joi.string().regex(/^[0-9]{7}$/).required(),
