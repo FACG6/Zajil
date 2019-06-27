45c84c781c6f9aba5f3b6c74b44c87f39a724a54
shorouq <shrooqabdullahsaad@gmail.com>
2019-06-27 09:56:51 +0300

    add to regex capital latter to name in add orders

--- server/controller/utils/addOrderValadation.js
+++ server/controller/utils/addOrderValadation.js
@@ -5,7 +5,7 @@ exports.schema = Joi.object().keys({
   address: Joi.string().required(),
   items: Joi.required(),
   phone: Joi.string().regex(/^\+?[0-9]{10,12}$/).required(),
-  customerName: Joi.string().regex(/^([أ-يa-z0-9]|\s)*$/).required(),
+  customerName: Joi.string().regex(/^([أ-يa-zA-Z0-9]|\s)*$/).required(),
   placeId: Joi.number().required(),
   captainId: Joi.number().required(),
 
